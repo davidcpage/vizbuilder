af7e2602ac9b7b379ca2383393f81a286bd8d389
David Page <davidpage@Davids-MacBook-Pro.local>
2025-07-24 16:44:41 +0100

    more general require func

--- src/vizbuilder/__init__.py
+++ src/vizbuilder/__init__.py
@@ -6,11 +6,11 @@ __version__ = "0.1.0"
 __author__ = "David Page"
 
 from .core.html_builder import Html
-from .components import require_d3
+from .components import require
 
 
 __all__ = [
     "Html",
-    'require_d3',
+    'require',
     # Add other components or utilities here as needed
 ]
--- src/vizbuilder/components/__init__.py
+++ src/vizbuilder/components/__init__.py
@@ -1,22 +1,6 @@
 from pathlib import Path
 from functools import cached_property
-
-def require_d3(user_code):  
-    return f"""
-    require.config({{
-        paths: {{
-            d3: "https://d3js.org/d3.v7.min"
-        }},
-        shim: {{
-            d3: {{
-                exports: "d3"
-            }}
-        }}
-    }});
-    require(["d3"], function(d3) {{
-        {user_code}
-    }});
-    """
+import json
 
 
 class _ComponentLoader:
@@ -32,4 +16,24 @@ _loader = _ComponentLoader()
 
 gantt_chart = _loader.gantt_chart
 
-__all__ = ['require_d3', 'gantt_chart']
+def require(modules=["d3"], config=None, user_code=""):
+    if config is None:
+        config = {
+            "paths": {
+                "d3": "https://d3js.org/d3.v7.min"
+            },
+            "shim": {
+                "d3": {
+                    "exports": "d3"
+                }
+            }
+        }
+
+    return f"""
+    require.config({json.dumps(config)});
+    require({json.dumps(modules)}, function({", ".join(modules)}) {{
+        {user_code}
+    }});
+    """
+
+__all__ = ['require', 'gantt_chart']
